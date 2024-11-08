9af4b2c8d3017c612111579a00af33233a763326
PrankMyCode123 <pan05102004@gmail.com>
2024-11-08 23:54:45 +0700

    Add files via upload

---
+++ ImageLoader.swift
@@ -0,0 +1,24 @@
+//
+//  ImageLoader.swift
+//  weatherApp
+//
+//  Created by Phương An on 08/11/2024.
+//
+
+import Foundation
+class ImageLoader : ObservableObject{
+    @Published var downloadedData : Data?
+    func downloadImage(url:String){
+        guard let imageURL = URL(string:url) else {
+            return
+        }
+        URLSession.shared.dataTask(with: imageURL){data,_,error in
+            guard let data = data , error == nil else{
+                return
+            }
+            DispatchQueue.main.async{
+                self.downloadedData = data
+            }
+        }.resume()
+    }
+}
